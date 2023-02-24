32241b208b987674b39d3367814954a497db0528
Alex Ainslie <alex.ainslie99@gmail.com>
2023-02-24 22:42:34 +0000

    Color edits for HTML template

--- src/page-template.js
+++ src/page-template.js
@@ -9,8 +9,8 @@ const generateTeam = team => {
                             <h2 class="card-title">${manager.getName()}</h2>
                             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
                         </div>
-                        <div class="card-body">
-                            <ul class="list-group">
+                        <div class="card-body bg-light">
+                            <ul class="list-group bg-white">
                                 <li class="list-group-item">ID: ${manager.getId()}</li>
                                 <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
                                 <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
@@ -28,8 +28,8 @@ const generateTeam = team => {
                             <h2 class="card-title">${engineer.getName()}</h2>
                             <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
                         </div>
-                        <div class="card-body">
-                            <ul class="list-group">
+                        <div class="card-body bg-light">
+                            <ul class="list-group bg-white">
                                 <li class="list-group-item">ID: ${engineer.getId()}</li>
                                 <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
                                 <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
@@ -47,8 +47,8 @@ const generateTeam = team => {
                             <h2 class="card-title">${intern.getName()}</h2>
                             <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
                         </div>
-                        <div class="card-body">
-                            <ul class="list-group">
+                        <div class="card-body bg-light">
+                            <ul class="list-group bg-white">
                                 <li class="list-group-item">ID: ${intern.getId()}</li>
                                 <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
                                 <li class="list-group-item">School: ${intern.getSchool()}</li>
